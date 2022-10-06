6bb1de5e209d8d3841593e64b0e469a0625f7217
Tuhin Saikhe <tuhinsaikh2@gmail.com>
2022-10-06 22:34:21 +0530

    Pagination sorting added

--- BlogApp/src/main/java/com/blogapp/service/BlogService.java
+++ BlogApp/src/main/java/com/blogapp/service/BlogService.java
@@ -11,5 +11,5 @@ public interface BlogService {
 	public Blog updateBlogById(Blog blog,Integer blogId);
 	public Blog getBlogById(Integer blogId);
 	public String deleteBlogById(Integer blogId);
-	public List<Blog> getBlogsPaginatingAndSorting(Integer pageSize, Integer pageNo, String sortBy);
+	public List<Blog> getBlogsPaginatingAndSorting(Integer pageSize, Integer pageNo, String sortBy, String sortDirection);
 }
